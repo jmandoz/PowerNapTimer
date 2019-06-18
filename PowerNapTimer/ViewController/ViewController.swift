5dfcd33f6e338094daa2461beb6803ffb3f046e6
Jason Mandozzi <jason.mandozzi@gmail.com>
2019-06-18 10:52:23 -0600

    Views layed out

---
+++ PowerNapTimer/ViewController/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  PowerNapTimer
+//
+//  Created by Jason Mandozzi on 6/18/19.
+//  Copyright © 2019 Jason Mandozzi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var napButton: UIButton!
+    
+    let timer = Timer()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func buttonTapped(_ sender: Any) {
+    }
+    
+    func updateLabel() {
+        
+    }
+    
+    func updateButton() {
+        
+    }
+    
+    func startTimer() {
+        
+    }
+    
+    func stopTimer() {
+        
+    }
+    
+    func setTimer() {
+        
+    }
+    
+    
+    
+}
+
